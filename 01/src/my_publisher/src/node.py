3b27d8200d9fb3c141ef76c79d70925a5c12d228
Tom Jelen <tom@jelen.dk>
2017-10-19 18:59:03 +0200

    publish a message to a topic

--- 01/src/my_publisher/src/node.py
+++ 01/src/my_publisher/src/node.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python
+"""Publishes a message periodically"""
+
 import datetime
+
 import rospy
+import std_msgs
 
 rospy.init_node('my_publisher')
 rate = rospy.Rate(10)
+world = rospy.Publisher('jelen/holas', std_msgs.msg.String, queue_size=10)
 
 while not rospy.is_shutdown():
-    print str(datetime.datetime.now()) + ' -> hola!'
+    message = 'hola @ ' + str(datetime.datetime.now())
+    print 'publishing -> ' + message
+    world.publish(message)
     rate.sleep()
