8f0d8e56797bb8b424f144f81209249db24c0e7d
Tom Jelen <tom@jelen.dk>
2017-10-21 23:30:20 +0200

    add super simple action server

--- 02/src/servant/src/node.py
+++ 02/src/servant/src/node.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python
+import time
 
+import actionlib
+import rospy
+
+from servant.msg import WorkAction, WorkGoal, WorkResult
+
+def do_work(work):
+    print 'Working on: ' + work.task_name
+    result = WorkResult()
+    result.time_elapsed = rospy.Duration.from_sec(1000)
+
+    server.set_succeeded(result)
+
+rospy.init_node('servant')
+server = actionlib.SimpleActionServer('worker', WorkAction, do_work, False)
+server.start()
+rospy.spin()
